dfa6f02f723fa65f0d8482fcb114e1466fe00c3b
Gagandeep Kaur <gagandeepkaur7099@gmail.com>
2026-07-05 12:47:51 +0530

    add python dictionary example

---
+++ Python/09_dictionary.py
@@ -0,0 +1,9 @@
+student = {
+  "name" : "Gagandeep kaur ",
+  "branch" : "CSE" ,
+  "year" : 3 ,
+  "college": "Sri Sukhmani Institute of Engineering and Technology" }
+
+print(student)
+print(student ["name"])
+print(student["branch"])  
