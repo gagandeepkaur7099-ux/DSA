fb1e02979600f7a362dafd679f93bd002d4b9251
Gagandeep Kaur <gagandeepkaur7099@gmail.com>
2026-07-16 14:39:54 +0530

    Add string slicing

---
+++ Python/13_String_slicing.py
@@ -0,0 +1,7 @@
+name = "Gagandeep kaur"
+
+print("Original:", name)
+print("First 5 letters:", name[0:5])
+print("Last 4 letters:", name[-4:])
+print("First character:", name[0])
+print("Last character:", name[-1])
