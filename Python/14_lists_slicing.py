38a54d9e48470f7450cd0e1f99681970cfa81eab
Gagandeep Kaur <gagandeepkaur7099@gmail.com>
2026-07-16 15:36:32 +0530

    Add examples demonstrating list slicing in Python

---
+++ Python/14_lists_slicing.py
@@ -0,0 +1,6 @@
+numbers = [10, 20, 30, 40, 50, 60]
+
+print("Original List:", numbers)
+print("First 3 Elements:", numbers[:3])
+print("Last 3 Elements:", numbers[-3:])
+print("Middle Elements:", numbers[2:5])
