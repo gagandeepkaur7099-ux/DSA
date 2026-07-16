0d0c84ca46f175bf392c8f555d03fb9927beaeee
Gagandeep Kaur <gagandeepkaur7099@gmail.com>
2026-07-16 14:19:32 +0530

    add basic python String operations

---
+++ Python/12_strings.py
@@ -0,0 +1,8 @@
+# String operations
+ name = "Gagandeep kaur"
+print ("Original String :", name)
+print ("Upper Case :" , name.upper())
+print ("Lower Case :" , name.lower())
+print("length :" , len (name))
+print("First Character :" , name [0])
+print("Last character:" , name[-1])
